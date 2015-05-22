c1345ed0baec6aef33e6f77317e7e12a4cf06461
Edoardo Gargano <garganoe@MOBTEK69.yoox.net>
2015-05-22 12:45:49 +0200

    no message

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1--------------------------------------
+
+var testObj = {
+    "name": "ciao",
+    surname: "arrivedereci",
+    address: 1,
+    "city": bologna
+};
+
+// 2--------------------------------------
+
+testObj['name'];
+debug;
+testObj.name;
+debugger;
+
+// 3--------------------------------------
+
+1=="1"
+1==="1"
+"1"==="1"
+1=1
+
+//deferred?
+
+// jquery 2 novità?
+// zepto?
+
+// backbone?
+// chainability?

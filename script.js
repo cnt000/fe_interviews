aab89c362f2b43688d77a49a91540072d9c1babf
Edoardo Gargano <edoardo.gargano@yoox.com>
2015-07-23 13:06:03 +0200

    js a caso da web

--- script.js
+++ script.js
@@ -21,10 +21,69 @@ debugger;
 "1"==="1"
 1=1
 
-//deferred?
+//--------------------------
+var nodes = document.getElementsByTagName('div');
+for (var i = 0; i < nodes.length; i++) {
+   nodes[i].addEventListener('click', function() {
+      console.log('You clicked element #' + i);
+   });
+}
+-----
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof undefined);
+-----
+var foo = function() {
+  alert("Hello World!");
+};
 
-// jquery 2 novità?
-// zepto?
+var bar = function(arg) {
+  return arg;
+};
 
-// backbone?
+bar(foo)();
+----
+function add(value1) {
+  return function doAdd(value2) {
+    return value1 + value2;
+  };
+}
+
+var increment = add(1);
+var foo = increment(2);
+// foo equals 3
+-----
+function Person(name) {
+  var _name = name;
+
+  this.getName = function() {
+    return _name;
+  };
+}
+-----
+(function() {
+   var a = b = 5;
+})();
+ 
+console.log(b);
+-----
+var fullname = 'John Doe';
+var obj = {
+   fullname: 'Colin Ihrig',
+   prop: {
+      fullname: 'Aurelio De Rosa',
+      getFullname: function() {
+         return this.fullname;
+      }
+   }
+};
+
+console.log(obj.prop.getFullname());
+ 
+var test = obj.prop.getFullname;
+ 
+console.log(test());
+
+// MV*?
 // chainability?
